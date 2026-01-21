5d9a40f255cc90c798b522c2739b202a1983ac7f
Libori-cyborg <arnaulibori7@gmail.com>
2026-01-21 08:53:13 +0100

    Add files via upload

--- ServicioWebDNSiDHCP-main/Backend/script.py
+++ ServicioWebDNSiDHCP-main/Backend/script.py
@@ -5,12 +5,15 @@ import os
 import ipaddress
 from datetime import datetime
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 app = Flask(
     __name__,
-    template_folder="../frontend",
-    static_folder="../frontend",
+    template_folder=os.path.join(BASE_DIR, "..", "Frontend"),
+    static_folder=os.path.join(BASE_DIR, "..", "Frontend"),
     static_url_path=""
 )
+
 CORS(app)
 
 # ---------- Utilitats ----------
